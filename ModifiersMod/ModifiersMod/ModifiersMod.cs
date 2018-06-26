853d53745c84bb9eaa816f98c0ceae7284539e4a
Mason Livingston <mlivingston@virtusgroup.ca>
2018-06-26 12:19:37 -0600

    ModTek compatibility

--- ModifiersMod/ModifiersMod/ModifiersMod.cs
+++ ModifiersMod/ModifiersMod/ModifiersMod.cs
@@ -1,15 +1,36 @@
 ﻿using Harmony;
+using Newtonsoft.Json;
+using System;
 using System.Reflection;
 
 namespace ModifiersMod
 {
-    class ModifiersMod
+
+    public class Settings
     {
-        public static void Init(string directory, string settingsJSON)
+        public float ChangeAmount = 0f;
+    }
+
+    public static class ModifiersMod
+    {
+        public static Settings settings;
+        public static void Init(string modDirectory, string settingsJson)
         {
             var harmony = HarmonyInstance.Create("ca.gnivler.ModifiersMod");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
 
+            try
+            {
+                // deserialize settings json onto our settings object
+                settings = JsonConvert.DeserializeObject<Settings>(settingsJson);
+            }
+            catch (Exception)
+            {
+                // use default settings
+                settings = new Settings();
+            }
+
+            //...
         }
     }
-}
+} 
--- ModifiersMod/ModifiersMod/Patch_GetAllModifiers.cs
+++ ModifiersMod/ModifiersMod/Patch_GetAllModifiers.cs
@@ -1,20 +1,23 @@
 ﻿using BattleTech;
 using Harmony;
 using UnityEngine;
+using Newtonsoft.Json;
+using System;
 
 namespace ModifiersMod
 {
+    
     [HarmonyPatch(typeof(ToHit), "GetAllModifiers")]
     public static class Patch_GetAllModifiers
     {
-
+        
         static bool Prefix()
         {
             return false;
         }
 
         static void Postfix(ToHit __instance, AbstractActor attacker, Weapon weapon, ICombatant target, Vector3 attackPosition, Vector3 targetPosition, LineOfFireLevel lofLevel, bool isCalledShot)
-        {        
+        {
             bool flag = lofLevel < LineOfFireLevel.LOFObstructed && weapon.IndirectFireCapable;
             float rangeModifier = __instance.GetRangeModifier(weapon, attackPosition, targetPosition);
             float coverModifier = __instance.GetCoverModifier(attacker, target, lofLevel);
@@ -38,10 +41,11 @@ namespace ModifiersMod
             float targetDirectFireModifier = __instance.GetTargetDirectFireModifier(target, flag);
             float indirectModifier = __instance.GetIndirectModifier(attacker, flag);
             float moraleAttackModifier = __instance.GetMoraleAttackModifier(target, isCalledShot);
+            moraleAttackModifier += ModifiersMod.settings.ChangeAmount;
             float num = rangeModifier + coverModifier + selfSpeedModifier + selfSprintedModifier + selfArmMountedModifier + stoodUpModifier + heightModifier + heatModifier + targetTerrainModifier + selfTerrainModifier + targetSpeedModifier + selfDamageModifier + targetSizeModifier + targetShutdownModifier + targetProneModifier + weaponAccuracyModifier + attackerAccuracyModifier + enemyEffectModifier + refireModifier + targetDirectFireModifier + indirectModifier + moraleAttackModifier;
-
-            var foo = Traverse.Create(__instance).Property("combat").GetValue<CombatGameState>();
-            if (num < 0f && !  foo.Constants.ResolutionConstants.AllowTotalNegativeModifier)
+          
+            CombatGameState combat = Traverse.Create(__instance).Property("combat").GetValue<CombatGameState>();
+            if (num < 0f && !combat.Constants.ResolutionConstants.AllowTotalNegativeModifier)
             {
                 num = 0f;
             }
