fdc71390b79a4b0fb191ca3c5ca4a928daf69868
Mason Livingston <mlivingston@virtusgroup.ca>
2018-06-26 13:28:24 -0600

    trying property patch

---
+++ ModifiersMod/ModifiersMod/Patch_CalledShotBonusModifierProperty.cs
@@ -0,0 +1,17 @@
+﻿using BattleTech;
+using Harmony;
+
+namespace ModifiersMod
+{
+    [HarmonyPatch(typeof(ToHit))]
+    [HarmonyPatch("CalledShotBonusMultiplier", PropertyMethod.Getter)]
+    class Patch_CalledShotBonusModifierProperty
+    {
+        static void Postfix(ref float __result)
+        {
+            // ChangeAmount here would be a percentage of increase or decrease to the game-calculated float
+            //
+            __result *= ModifiersMod.settings.ChangeAmount;
+        }
+    }
+}
